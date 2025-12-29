1887fbdfdae06dc3aa6d1329eafae28d121a9453
Lakshyaaa <iam.lakshya123@gmail.com>
2025-12-29 16:28:05 +0530

    fixed script

--- ascii-script.sh
+++ ascii-script.sh
@@ -3,4 +3,4 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, Im a DRAGON... GitHub Actions is awesome!" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
-ls ltra
+ls -ltra
