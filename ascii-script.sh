9d78eb4d82f8e59d88b6d0c9b82ec86e83ad19a8
Lakshyaaa <iam.lakshya123@gmail.com>
2025-12-29 16:24:32 +0530

    Executing shell script

---
+++ ascii-script.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, Im a DRAGON... GitHub Actions is awesome!" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls ltra
